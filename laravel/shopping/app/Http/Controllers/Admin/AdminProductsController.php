e1677dc9b421cad53f4474285fc084bb81c508bf
Yosuke, Tsutsumi | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2021-01-11 13:47:14 +0900

    complete admin

--- laravel/shopping/app/Http/Controllers/Admin/AdminProductsController.php
+++ laravel/shopping/app/Http/Controllers/Admin/AdminProductsController.php
@@ -6,6 +6,7 @@ use App\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
@@ -55,5 +56,64 @@ class AdminProductsController extends Controller
         } else {
             return 'No Image was selected';
         }
+    }
+
+
+    public function updateProduct(Request $request, $id) {
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $type = $request->input('type');
+        $price = $request->input('price');
+
+        $updateArray = array("name"=>$name, "description"=>$description, "type"=>$type, "price"=>$price);
+
+        DB::table('products')->where('id', $id)->update($updateArray);
+
+        return redirect()->route('adminDisplayProducts');
+    }
+
+    public function createProductForm() {
+
+        return view('admin.createProductForm');
+    }
+
+    // send new product to database
+    public function sendCreateProductForm(Request $request) {
+        $name = $request->input('name');
+        $description = $request->input('description');
+        $type = $request->input('type');
+        $price = $request->input('price');
+
+        Validator::make($request->all(), ['image' => 'required|image|mimes:jpg,png,jpeg|max:5000'])->validate();
+        $ext = $request->file('image')->getClientOriginalExtension(); //jpg
+        $stringImageReFormat = str_replace(" ", "", $request->input('name'));
+
+        $imageName = $stringImageReFormat . "." . $ext; //blackdress.jpg (i.e)
+        $imageEncoded = File::get($request->image);
+        Storage::disk("local")->put('public/product_images/'. $imageName, $imageEncoded);
+
+        $newProductArray = array("name"=>$name, "description"=>$description, "image" => $imageName, "type"=>$type, "price"=>$price);
+        $created = DB::table("products")->insert($newProductArray);
+
+        if($created) {
+            return redirect()->route("adminDisplayProducts");
+        } else {
+            return "Product was not created";
+
+        }
+    }
+
+    // delete product
+    public function deleteProduct($id){
+
+        $product = Product::find($id);
+        $exists = Storage::disk('local')->exists('public/product_images/'.$product->image);
+        // delete image
+        if($exists) {
+            Storage::delete('public/product_images'.$product->image);
+        }
+
+        Product::destroy($id);
+        return redirect()->route("adminDisplayProducts");
     }
 }
--- laravel/shopping/routes/web.php
+++ laravel/shopping/routes/web.php
@@ -40,3 +40,15 @@ Route::get('admin/editProductImageForm/{id}', ['uses' => 'Admin\AdminProductsCon
 
 // update product image
 Route::post('admin/updateProductImage/{id}', ['uses' => 'Admin\AdminProductsController@updateProductImage', 'as' => 'adminUpdateProductImage']);
+
+// update product data
+Route::post('admin/updateProduct/{id}', ['uses' => 'Admin\AdminProductsController@updateProduct', 'as' => 'adminUpdateProduct']);
+
+// display create product form
+Route::get('admin/createProductForm', ['uses' => 'Admin\AdminProductsController@createProductForm', 'as' => 'adminCreateProductForm']);
+
+// Send new product data to database
+Route::post('admin/createProductForm', ['uses' => 'Admin\AdminProductsController@sendCreateProductForm', 'as' => 'adminSendCreateProductForm']);
+
+// delete product
+Route::get('admin/deleteProduct/{id}', ['uses' => 'Admin\AdminProductsController@deleteProduct', 'as' => 'adminDeleteProduct']);
