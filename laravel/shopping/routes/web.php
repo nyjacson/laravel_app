77e2de8de199c0171552d14803663f875f034bf2
Yosuke, Tsutsumi | Oscar | TRVDD <yosuke.tsutsumi@rakuten.com>
2021-01-11 14:16:34 +0900

    add auth middleware

--- laravel/shopping/routes/web.php
+++ laravel/shopping/routes/web.php
@@ -30,7 +30,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 // Admin Panel
-Route::get('admin/products', ['uses' => 'Admin\AdminProductsController@index', 'as' => 'adminDisplayProducts']);
+Route::get('admin/products', ['uses' => 'Admin\AdminProductsController@index', 'as' => 'adminDisplayProducts'])->middleware('restrictToAdmin');
 
 // display edit product form
 Route::get('admin/editProductForm/{id}', ['uses' => 'Admin\AdminProductsController@editProductForm', 'as' => 'adminEditProductForm']);
